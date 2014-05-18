bfbe1cd23c316a657f2af76c79f627d1e2dc4173
Michael McLaughlin <michaelmc@gmail.com>
2014-05-18 14:46:06 -0400

    Added docstrings

--- boxfolders.py
+++ boxfolders.py
@@ -2,20 +2,38 @@ import requests
 
 class Folders:
     def __init__(self, token):
+        """
+        Initializer for the Folders class. Takes a bearer token as input and
+        sets the root folder of the object to be the user's 'All Files' Box
+        folder.
+
+        Returns False if there is a problem communicating with Box, else True.
+        """
         self.token = token
         self.folders = []
-        f = requests.get('https://api.box.com/2.0/folders/0/', headers = { 'Authorization': 'Bearer ' + token })
+        f = requests.get('https://api.box.com/2.0/folders/0/', 
+            headers = { 'Authorization': 'Bearer ' + token })
         if (f.status_code != 200): return None
         self.folders.append(f.json())
         self.current_folder = self.folders[-1]
+        return True
 
     def path(self):
-        s = ''
+        """
+        Returns the current path of the Folder object in normal /-delimited
+        form.
+        """
+        s = '/'
         for folder in self.folders:
             s += folder.get('name') + '/'
         return s
 
     def traverse(self, path, local=None):
+        """
+        Traverses a relative or absolute path from the Box 'All Files' root.
+        
+        Returns True if the entire path can be traversed, and False otherwise.
+        """
         path = path.split('/')
         if (path[0] == ''):
             self.folders = [self.folders[0]]
@@ -38,12 +56,19 @@ class Folders:
                 return False
 
     def item_type(self, item):
+        """Returns the type of a specified item."""
         for entry in self.current_folder.get('item_collection').get('entries'):
             if (entry.get('name') == item):
                 return entry.get('type')
         return None
 
     def list(self):
+        """
+        Lists out the file and folder contents of the current folder.
+
+        Returns a dict with filenames as keys and tuples of file id and file
+        type as the values.
+        """
         item_dict = {}
         for entry in self.current_folder.get('item_collection').get('entries'):
             item_type = entry.get('type')
@@ -53,6 +78,15 @@ class Folders:
         return item_dict
 
     def down(self, child, destination=None):
+        """
+        Moves down one level. If the specified child is a folder, that becomes
+        the new current folder. If the child is a file, that file is
+        downloaded. The destination to which the file is downloaded is an
+        optional argument.
+
+        Returns True if the specified file or folder is reached, False
+        otherwise.
+        """
         item_id = None
         for entry in self.current_folder.get('item_collection').get('entries'):
             if (entry.get('name') == child):
@@ -62,16 +96,17 @@ class Folders:
         if (item_id == None):
             print 'Requested item not found'
             return False
-
         if (item_type == 'folder'):
-            f = requests.get('https://api.box.com/2.0/folders/' + item_id + '/', headers = { 'Authorization': 'Bearer ' + self.token })
+            f = requests.get('https://api.box.com/2.0/folders/' + item_id + '/',
+                headers = { 'Authorization': 'Bearer ' + self.token })
             if (f.status_code != 200): 
                 print "Folder couldn't be opened"
                 return False
             self.folders.append(f.json())
             self.current_folder = self.folders[-1]
         elif (item_type == 'file'):
-            f = requests.get('https://api.box.com/2.0/files/' + item_id + '/content/', headers = { 'Authorization': 'Bearer ' + self.token })
+            f = requests.get('https://api.box.com/2.0/files/' + item_id + '/content/',
+                headers = { 'Authorization': 'Bearer ' + self.token })
             if (f.status_code != 200): 
                 print "File couldn't be opened"
                 return False
@@ -93,6 +128,11 @@ class Folders:
             return False
 
     def up(self):
+        """
+        Moves current folder up one level from present.
+
+        Returns True if successful, False if the at the top already.
+        """
         if (self.current_folder == self.folders[0]):
             print "Already at All Files"
             return False
@@ -101,17 +141,25 @@ class Folders:
             current_folder = self.folders[-1]
             return True
 
-    def upload(self, filename):
+    def upload(self, path):
+        """
+        Uploads a file to the current folder. Takes a file location as an
+        argument, either relative or absolute. If a file of the same name is
+        already in the current folder, a new version is uploaded.
+
+        Returns True if file upload is successful, False otherwise.
+        """
         parent_id = self.current_folder.get('id')
         item_id = None
+        filename = path.split('/')[-1]
         for entry in self.current_folder.get('item_collection').get('entries'):
             if (entry.get('name') == filename and entry.get('type') == 'file'):
                 item_id = entry.get('id')
                 break
         if (item_id == None):
-            f = requests.post('https://upload.box.com/api/2.0/files/content', headers = { 'Authorization': 'Bearer ' + self.token }, data = { 'filename': filename, 'parent_id':parent_id }, files = { filename: open(filename, 'rb')})
+            f = requests.post('https://upload.box.com/api/2.0/files/content', headers = { 'Authorization': 'Bearer ' + self.token }, data = { 'filename': filename, 'parent_id':parent_id }, files = { filename: open(path, 'rb')})
         else:
-            f = requests.post('https://upload.box.com/api/2.0/files/' + item_id + '/content', headers = { 'Authorization': 'Bearer ' + self.token }, data = { 'filename': filename }, files = { filename: open(filename, 'rb')})
+            f = requests.post('https://upload.box.com/api/2.0/files/' + item_id + '/content', headers = { 'Authorization': 'Bearer ' + self.token }, data = { 'filename': filename }, files = { filename: open(path, 'rb')})
         if (f.status_code == 409):
             print "File upload caused a conflict"
             return False
